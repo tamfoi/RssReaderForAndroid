3ed1c3c40c18f79d037022cf3df49063f462d640
tamfoi <tamfoi@users.noreply.github.com>
2019-06-30 20:37:47 +0900

    リスト表示できるようにした

---
+++ app/src/main/java/io/github/tamfoi/rssreaderforandroid/AsyncRssLoader.kt
@@ -0,0 +1,118 @@
+package io.github.tamfoi.rssreaderforandroid
+
+import android.os.AsyncTask
+import android.util.Log
+import android.util.Xml
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import java.io.StringReader
+
+class AsyncRssLoader: AsyncTask<Void, Void, String>() {
+
+    var url: String? = null
+    var onSuccess: (() -> Unit)? = null
+    val result = mutableListOf<HashMap<String,String>>()
+
+    override fun doInBackground(vararg p0: Void?): String? {
+        if(this.url != null){
+            val client = OkHttpClient()
+            val request = Request.Builder().url(this.url).get().build()
+            val response = client.newCall(request).execute()
+            return response.body()!!.string()
+        } else {
+            return null
+        }
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+        if(result != null){
+            try {
+                val xmlPullParser = Xml.newPullParser()
+                xmlPullParser.setInput(StringReader(result))
+
+                var eventType = xmlPullParser.eventType
+
+                var type = ""
+                var title = ""
+                var link = ""
+                var isItemTag = false
+                var isTitleTag = false
+                var isLinkTag = false
+
+                while (eventType != XmlPullParser.END_DOCUMENT){
+                    when(eventType){
+                        XmlPullParser.START_DOCUMENT -> {}
+                        XmlPullParser.START_TAG -> {
+
+                            when(xmlPullParser.name) {
+                                //RSSのタイプを判定
+                                "rss" -> {
+                                    type = "rss"
+                                }
+                                "feed" -> {
+                                    type = "atom"
+                                }
+                            }
+
+                            //記事アイテムのタグに到達したらフラグを立てる
+                            if((type == "rss" && xmlPullParser.name == "item") || (type == "atom" && xmlPullParser.name == "entry")){
+                                isItemTag = true
+                            }
+
+                            if(isItemTag && xmlPullParser.name == "title") {
+                                isTitleTag = true
+                            }
+
+                            if(isItemTag && xmlPullParser.name == "link") {
+                                isLinkTag = true
+
+                                if(type == "atom"){
+                                    link = xmlPullParser.getAttributeValue("", "href")
+                                }
+                            }
+                        }
+                        XmlPullParser.END_TAG -> {
+
+                            //記事アイテムのタグの終端まで到達したらフラグを下げる
+                            if((type == "rss" && xmlPullParser.name == "item") || (type == "atom" && xmlPullParser.name == "entry")){
+                                val item = HashMap<String,String>()
+                                item.put("title", title)
+                                item.put("link", link)
+                                this.result.add(item)
+                                isItemTag = false
+                                title = ""
+                                link = ""
+                            }
+
+                            if(isItemTag && xmlPullParser.name == "title") {
+                                isTitleTag = false
+                            }
+
+                            if(isItemTag && xmlPullParser.name == "link") {
+                                isLinkTag = false
+                            }
+
+                        }
+                        XmlPullParser.TEXT -> {
+                            if(isTitleTag){
+                                title = xmlPullParser.text
+                            }
+                            if(isLinkTag && type == "rss"){
+                                link = xmlPullParser.text
+                            }
+                        }
+                    }
+                    eventType = xmlPullParser.next()
+                }
+
+                if(onSuccess != null){
+                    onSuccess?.invoke()
+                }
+
+            } catch (e: Exception) {}
+        }
+    }
+
+}
--- app/src/main/java/io/github/tamfoi/rssreaderforandroid/MainActivity.kt
+++ app/src/main/java/io/github/tamfoi/rssreaderforandroid/MainActivity.kt
@@ -1,52 +1,39 @@
 package io.github.tamfoi.rssreaderforandroid
 
-import android.os.AsyncTask
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.util.Xml
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.xmlpull.v1.XmlPullParser
-import java.io.StringReader
+import android.widget.SimpleAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AsyncLoadRss().execute()
-    }
 
-    inner class AsyncLoadRss: AsyncTask<Void, Void, String>() {
+        var asyncRssLoader = AsyncRssLoader()
+        asyncRssLoader.url = "https://tamfoi.hatenablog.com/rss"
+        asyncRssLoader.onSuccess = {
+            val adapter = SimpleAdapter(
+                this,
+                asyncRssLoader.result,
+                android.R.layout.simple_list_item_2,
+                arrayOf("title", "link"),
+                intArrayOf(android.R.id.text1, android.R.id.text2)
+            )
 
-        override fun doInBackground(vararg p0: Void?): String? {
-            val client = OkHttpClient()
-            val request = Request.Builder().url("https://tamfoi.hatenablog.com/rss").get().build()
-            val response = client.newCall(request).execute()
-            return response.body()!!.string()
+            itemListView.adapter = adapter
+
+            itemListView.setOnItemClickListener {parent, view, position, id ->
+                val uri = Uri.parse(asyncRssLoader.result[position]["link"])
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
         }
+        asyncRssLoader.execute()
 
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            try {
-                val xmlPullParser = Xml.newPullParser()
-                xmlPullParser.setInput(StringReader(result))
-
-                var eventType = xmlPullParser.eventType
-                while (eventType != XmlPullParser.END_DOCUMENT){
-                    if(eventType == XmlPullParser.START_DOCUMENT) {
-                        Log.d("MyDebugger", "Start document")
-                    } else if(eventType == XmlPullParser.START_TAG) {
-                        Log.d("MyDebugger", "Start tag "+xmlPullParser.name)
-                    } else if(eventType == XmlPullParser.END_TAG) {
-                        Log.d("MyDebugger", "End tag "+xmlPullParser.name)
-                    } else if(eventType == XmlPullParser.TEXT) {
-                        Log.d("MyDebugger", "Text "+xmlPullParser.text)
-                    }
-                    eventType = xmlPullParser.next()
-                }
-            } catch (e: Exception) {}
-        }
     }
 }
